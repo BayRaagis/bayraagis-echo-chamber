c0cbb839e691aaaa0aecbe3a5a88ecdb623ba303
Neeraja Abhyankar <neeraja.abhyankar@gmail.com>
2025-07-10 23:35:23 -0700

    events page

---
+++ src/utils/eventsData.ts
@@ -0,0 +1,36 @@
+
+// TODO: add time, location
+
+export const pastEvents = [
+  {
+    id: 1,
+    title: "SF Indian Classical Sessions @ RootStock Arts",
+    date: "June 25, 2025",
+    link: "https://www.instagram.com/p/DKxfvXxzc5l/"
+  },
+  {
+    id: 2,
+    title: "Make Music Day San Jose",
+    date: "June 21, 2025",
+    link: "https://www.instagram.com/p/DKGoRJjgXhv/"
+  },
+  {
+    id: 3,
+    title: "Taranas & Thillanas",
+    date: "June 14, 2025",
+    link: "https://www.instagram.com/p/DKxsRvRzi13/"
+  },
+] as const;
+
+export const upcomingEvents = [
+  {
+    id: 1,
+    title: "Bhajans, Abhangas, and Shabads",
+    date: "July 12, 2025",
+    link: "https://www.instagram.com/p/DL2dxszxvVQ/"
+  }
+] as const;
+
+// export type pastPerformance = typeof pastEvents[number];
+// export type upcomingPerformance = typeof upcomingEvents[number];
+
