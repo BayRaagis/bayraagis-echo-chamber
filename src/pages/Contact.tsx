c19e46c66fb29ff56e154cedf3a1a9942c115020
Neeraja Abhyankar <neeraja.abhyankar@gmail.com>
2026-03-24 22:35:08 -0500

    performer interest form

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -43,96 +43,25 @@ const Contact = () => {
       <div className="container mx-auto py-12 px-4">
         <h1 className="text-4xl font-bold mb-8 text-center">Contact Us</h1>
 
-        <p>
-          Reach us at <a href="mailto:bayraagis24@gmail.com">bayraagis24@gmail.com</a>
-          <br/><br/>
-          We are open to show bookings and inquiries!<br/>
-          For collaborations or joining the collective, please send us links to some of your previous performances.<br/>
-          We look forward to hearing from you.
-        </p>
-        
-        {/* // TODO: make backend work */}
-        {/* <div className="max-w-lg mx-auto">
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-              <FormField
-                control={form.control}
-                name="name"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Your name" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input type="email" placeholder="Your email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              
-              <FormField
-                control={form.control}
-                name="purpose"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Purpose</FormLabel>
-                    <Select 
-                      onValueChange={field.onChange} 
-                      defaultValue={field.value}
-                    >
-                      <FormControl>
-                        <SelectTrigger>
-                          <SelectValue placeholder="Select purpose" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        <SelectItem value="join">Join the collective as a performer</SelectItem>
-                        <SelectItem value="book">Book BayRaagis for a show</SelectItem>
-                        <SelectItem value="other">Other</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              
-              <FormField
-                control={form.control}
-                name="message"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Message</FormLabel>
-                    <FormControl>
-                      <Textarea 
-                        placeholder="Your message..." 
-                        className="min-h-[150px]" 
-                        {...field} 
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              
-              <Button type="submit" className="w-full">
-                Submit
-              </Button>
-            </form>
-          </Form>
-        </div> */}
+        <div className="text-center">
+          We are open to show bookings and inquiries! Reach us at <a href="mailto:bayraagis24@gmail.com">bayraagis24@gmail.com</a>
+          <br /><br /><br />
+        </div>
+
+        <div className="max-w-lg mx-auto">
+          For collaborations or joining the collective, please fill out the form below:
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdFiQMxRNOQE8Y_WsP25SCC_vq-j60MpsE5rh-41Ou_4XBQTA/viewform?embedded=true"
+            width="100%"
+            height="1200"
+            frameBorder="0"
+            marginHeight={0}
+            marginWidth={0}
+          >
+            Loading…
+          </iframe>
+        </div>
+
       </div>
     </Layout>
   );
